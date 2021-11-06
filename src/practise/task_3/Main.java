12b2e94e3a5eac8d3efa174adce3d9ad7d4e3a2a
Anatoliy Morozov <anatoly.marozaw@gmail.com>
2021-11-06 13:50:18 +0300

    task_3

---
+++ src/practise/task_3/Main.java
@@ -0,0 +1,15 @@
+package practise.task_3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Versions: Java  5, Java 6, Java   7, Java 8.";
+        Pattern p = Pattern.compile("Java\s+[0-9]");
+        Matcher m = p.matcher(str);
+        while(m.find()){
+            System.out.println(m.group());
+        }
+    }
+}
